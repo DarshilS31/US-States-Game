2f576d8f124477e54210fcf5a14b6e7cbaa2d02f
Darshil Sharma <darshils3103@gmail.com>
2026-01-18 15:37:07 +0530

    Fix stop logic and polish game flow

--- main.py
+++ main.py
@@ -8,27 +8,29 @@ screen.bgpic("blank_states_img.gif")
 draw=turtle.Turtle(visible=False)
 print(data.state)
 game_not_over=True
-guess_no=0
+
 guessed=[]
 state_list=data["state"].to_list()
 while(game_not_over):
 
-    guess=screen.textinput(title=f"{guess_no}/50",prompt="Enter a US state:")
+    guess=screen.textinput(title=f"{len(guessed)}/50",prompt="Enter a US state:")
     if(guess is None):
         game_not_over=False
     else:
         guess=guess.title()
+    if(guess=="Stop" or len(guessed)==50):
+        game_not_over=False
+        break
     
     if((guess in state_list) and (guess not in guessed)):
         guessed.append(guess)
-        x_coor=float(data[data["state"]==guess].x)
-        y_coor=float(data[data["state"]==guess].y)
+        x_coor=data[data["state"]==guess].x.item()
+        y_coor=data[data["state"]==guess].y.item()
         draw.teleport(x_coor,y_coor)
-        draw.write(f"{guess}",font=("Arial",8,"normal"),align="center")
-        guess_no+=1
+        draw.write(guess,font=("Arial",8,"normal"),align="center")
 
-    if(guess=="Stop" or guess_no==50):
-        game_not_over=False
+
+    
 screen.mainloop()
 
 
